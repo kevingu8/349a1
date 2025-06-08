6707cccdcba19dbdaaf925b0916dcb4629ea2c03
AustingDong <a7dong@uwaterloo.ca>
2025-06-08 00:25:20 -0400

    finish bubble

---
+++ a1/src/drawbles/displaylist.ts
@@ -0,0 +1,19 @@
+import type { Drawable } from './drawable';
+
+export class DisplayList {
+  list: Drawable[] = [];
+
+  add(drawable: Drawable) {
+    this.list = [...this.list, drawable];
+  }
+
+  remove(drawable: Drawable) {
+    this.list = this.list.filter((d) => d !== drawable);
+  }
+
+  draw(gc: CanvasRenderingContext2D) {
+    this.list.forEach((d) => {
+      d.draw(gc);
+    });
+  }
+}
